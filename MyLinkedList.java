d33d1c78d84b4968099de4308608bc6f56f151f7
anuragdiwate <72479802+anuragdiwate@users.noreply.github.com>
2021-05-31 23:53:43 -0500

    Add files via upload

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,8 +1,9 @@
+import java.util.List;
+
 public class MyLinkedList {
         class SinglyListNode{
             int val;
             SinglyListNode next;
-            SinglyListNode prev;
             public SinglyListNode(){}
             public SinglyListNode (int x){ this.val = x;}
         }
@@ -36,18 +37,25 @@ public class MyLinkedList {
 
         /** Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. */
         public void addAtHead(int val) {
-
+            addAtIndex(0, val);
         }
 
         /** Append a node of value val to the last element of the linked list. */
         public void addAtTail(int val) {
             tail.next = new SinglyListNode(val);
+            ListSize++;
         }
 
         /** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
         public void addAtIndex(int index, int val) {
+            if (index == ListSize) {
+                addAtTail(val);
+                return;
+            }
             SinglyListNode hold = getNode(index).next;
             getNode(index).next = new SinglyListNode(val);
+            getNode(index).next.next = hold;
+            ListSize++;
         }
 
         /** Delete the index-th node in the linked list, if the index is valid. */
@@ -58,6 +66,17 @@ public class MyLinkedList {
         public void printList() {
 
         }
+
+    public static void main(String[] args) throws NullPointerException {
+
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.addAtHead(1);
+        myLinkedList.addAtTail(3);
+        myLinkedList.addAtIndex(1, 2);    // linked list becomes 1->2->3
+        myLinkedList.get(1);              // return 2
+        myLinkedList.deleteAtIndex(1);    // now the linked list is 1->3
+        myLinkedList.get(1);              // return 3
+    }
     }
 
 /**
